f925673f774b806357b97b1b8db7dcbfd1aa6d83
Eva Malluta <eva.malluta14@gmail.com>
2022-08-04 13:31:32 +0200

    change

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Products from './components/Products'
 import { Categories } from './components/Categories'
 import { ProductDetail } from './components/ProductDetail'
+import { PageNotFound } from './components/PageNotFound'
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Routes>
         <Route path='/products' element={<Products/>} /> 
         <Route path='/categories' element={<Categories/>} /> 
-        <Route path='/products:id' element={<ProductDetail/>} />
+        <Route path='/products/:id' element={<ProductDetail/>} />
+        {/* <Route path='*' element={<PageNotFound />} /> */}
 
         </Routes>
       </main>
